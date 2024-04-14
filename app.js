1ef1383c44a895a5487ddcdcea6637d08f059efb
pawan <pavandeore90@gmail.com>
2024-04-15 01:26:16 +0530

    meme added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 window.onload = function(){
     let addButton = document.getElementById('add');
     let todos = document.getElementById('todos');
+    let memeDiv = document.getElementById('meme-div');
     let storage = localStorage.getItem('store') || [];
+    const memeUrls = [
+        'https://media.makeameme.org/created/and-it-feel-5bec36.jpg',
+        'https://i.imgflip.com/4hb499.png?a475776',
+        'https://i.chzbgr.com/full/5671478784/h912E5930/feels-good-man',
+    ]
 
     if(storage.length){
         storage = JSON.parse(storage);
@@ -43,14 +49,25 @@ window.onload = function(){
             let val = e.target.nextElementSibling.innerText;
             let checked = e.target.checked;
 
-
             storage.forEach(entry => {
                 if(entry.value == val){
                     entry['done'] = checked;
                 }
             })
 
-            localStorage.setItem('store', JSON.stringify(storage))
+            localStorage.setItem('store', JSON.stringify(storage));
+
+            if(checked){
+                let tempImg = document.createElement('img')
+                tempImg.src = memeUrls[Math.floor(Math.random() * memeUrls.length)];
+                tempImg.height = "170";
+                tempImg.height = "170";
+                memeDiv.appendChild(tempImg);
+    
+                setTimeout(() => {
+                    memeDiv.innerHTML = "";
+                }, 3000);
+            }
         })
     })
 
